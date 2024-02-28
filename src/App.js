ccb996c3d22fa5ab172c5a5f2f39d269a21538af
ock2023 <dmsdhr2605@gmail.com>
2024-02-29 01:17:23 +0900

    tab

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import Event from "./pages/Event";
 import { useState } from "react";
 import data from "./data";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function App() {
-  const [shoes] = useState(data);
+  const [shoes, setShoes] = useState(data);
   const navigate = useNavigate();
 
   return (
@@ -34,7 +35,7 @@ function App() {
             </Nav.Link>
             <Nav.Link
               onClick={() => {
-                navigate("/detail/");
+                navigate("/detail/1");
               }}
             >
               Detail
@@ -69,7 +70,7 @@ function App() {
               <div className="container">
                 <div className="row">
                   {shoes.map((a, i) => {
-                    return <Card shoes={shoes[i]} i={i} />;
+                    return <Card shoes={shoes[i]} i={i} key={i} />;
                   })}
                 </div>
               </div>
@@ -90,6 +91,23 @@ function App() {
 
         <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
+      <button
+        onClick={() => {
+          axios
+            .get("https://codingapple1.github.io/shop/data2.json")
+            .then((결과) => {
+              console.log(결과.data);
+              // console.log(shoes);
+              let copy = [...shoes, ...결과.data];
+              setShoes(copy);
+            })
+            .catch(() => {
+              console.log("실패함");
+            });
+        }}
+      >
+        더보기
+      </button>
     </div>
   );
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Nav } from "react-bootstrap";
+import "../App.css";
+
+const Box = styled.div`
+  height: 100px;
+  width: 300px;
+  background-color: yellow;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Detail = (props) => {
+  const [box, setBox] = useState(true);
+  const [input, setInput] = useState("");
+  const [tabs, setTabs] = useState(0);
+  const [fade2, setFade2] = useState("");
+
+  useEffect(() => {
+    setFade2("end");
+    return () => {
+      setFade2("");
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(input) == true) {
+      alert("그러지마세요");
+    }
+  }, [input]);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setBox(false);
+
+      return () => {
+        clearTimeout(timer); // 기존 데이터 요청은 제거해주세요~
+      };
+    }, 2000);
+    // console.log(1)
+  });
+
   const { id } = useParams();
   // console.log(id)
   const findItem = props.shoes.find((x) => x.id === parseInt(id));
@@ -12,7 +52,9 @@ const Detail = (props) => {
 
   return (
     <div>
-      <div className="container">
+      <div className={"container start " + fade2}>
+        {box === true ? <Box>2초 이내 구매 시 할인</Box> : null}
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
         <div className="row">
           <div className="col-md-6">
             <img
@@ -28,8 +70,73 @@ const Detail = (props) => {
           </div>
         </div>
       </div>
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link0"
+            onClick={() => {
+              setTabs(0);
+            }}
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link1"
+            onClick={() => {
+              setTabs(1);
+            }}
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link2"
+            onClick={() => {
+              setTabs(2);
+            }}
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <TapContent tabs={tabs} />
     </div>
   );
 };
+
+// 컴포넌트는 리턴문이 꼭 있어야 하기에 return은 적어주어야 한다.
+function TapContent(props) {
+  let [fade, setFade] = useState("");
+
+  useEffect(() => {
+    const a = setTimeout(() => {
+      setFade("end");
+    }, 100);
+    return () => {
+      clearTimeout(a);
+      setFade("");
+    };
+  }, [props.tabs]);
+
+  return (
+    <div className={"start " + fade}>
+      {/* className명 뒤에 띄어쓰기를 해줘야 함 그래야 두 개 적용이 됨. 그래서 start 뒤에 공백칸 하나가 존재한다.*/}
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][props.tabs]}
+    </div>
+  );
+  // if (props.tabs === 0) {
+  //   return <div>내용0</div>;
+  // }
+  // if (props.tabs === 1) {
+  //   return <div>내용1</div>;
+  // }
+  // if (props.tabs === 2) {
+  //   return <div>내용2</div>;
+  // }
+}
 
 export default Detail;
